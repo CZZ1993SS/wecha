16966ba11dded5cdc75b5387a25ca7775fdcb4ee
CZZ <czz@gmail.com>
2019-05-15 01:20:59 +0800

    修复中文换行问题，新增图片放大功能

--- pages/movie/movie-detail.js
+++ pages/movie/movie-detail.js
@@ -17,60 +17,23 @@ Page({
     wx.request({
       url: "http://t.yushu.im/v2/movie/subject/" + movieId,
       success:function(data){
+        var movieDetail = data.data;
+        var content = movieDetail.summary.replace(/\\n/g,'\n');
+        movieDetail.summary = content;
         _that.setData({
-          movieDetail: data.data
+          movieDetail: movieDetail
         })
       }
 
     })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onPreviewImage: function(event){
+    var imageUrl = event.currentTarget.dataset.imageUrl;
+    wx.previewImage({
+      urls: [imageUrl] // 需要预览的图片http链接列表
+    })
   }
+
+
 })
